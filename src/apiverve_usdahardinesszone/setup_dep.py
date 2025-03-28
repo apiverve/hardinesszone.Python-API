6a8c2eaba4f62f0394078beff5287ae76b7795b2
APIVerve Team <hello@apiverve.com>
2025-03-28 17:23:29 -0500

    Bug Fixes

---
+++ src/apiverve_usdahardinesszone/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_usdahardinesszone',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Hardiness Zone is a simple tool for getting the USDA Plant Hardiness Zone for a given zip code',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
